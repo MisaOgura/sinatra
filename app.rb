86ac74fe5030e3970d5aba470d3f746db7e2593e
Misa Ogura <misa.ogura01@gmail.com>
2016-03-14 13:02:48 +0000

    #8 Remove <div> tag

--- app.rb
+++ app.rb
@@ -21,7 +21,5 @@ get '/check' do
 end
 
 get '/cat' do
-  "<div>
-  <img src='http://bit.ly/1eze8aE' style='border: 5px red dashed'>
-  </div>"
+  "<img src='http://bit.ly/1eze8aE' style='border: 5px red dashed'>"
 end
